91b746bba6eac24b0a351df2bf5e641fbc09580a
Martin Pakandl <nzdm.martin@seznam.cz>
2022-09-25 17:14:25 +0200

    skoro dodělaná žádost o úvěr

--- src/app/form-details/form-details.component.ts
+++ src/app/form-details/form-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
+import { HttpRequestsService } from '../http-requests.service';
 import { UserInfoService } from '../user-info.service';
 
 @Component({
@@ -8,23 +9,26 @@ import { UserInfoService } from '../user-info.service';
 })
 export class FormDetailsComponent implements OnInit, DoCheck {
 
-  constructor(private userInfoService: UserInfoService) { }
+  requestCalc;
+
+  constructor(private userInfoService: UserInfoService, private httpRequestsService: HttpRequestsService) { }
 
   infoAboutUserFromAPI = null;
-
-
-
 
   displayClick() {
     console.log(this.userInfoService.infoAboutUser.name)
     console.log(this.infoAboutUserFromAPI)
-  
   }
+  
   ngOnInit(): void {
   }
 
   ngDoCheck(): void {
-    this.infoAboutUserFromAPI = this.userInfoService.infoAboutUser
+    this.infoAboutUserFromAPI = {
+      ...this.userInfoService.infoAboutUser, 
+      ...this.httpRequestsService.calculationInfo};
+
+      console.log(this.infoAboutUserFromAPI)
   }
 
 }
